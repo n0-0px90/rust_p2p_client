b69b40ddc7ae44cb3298aa3a0f3b0c2c9bc510ff
cameron.nichols <cameron.nichols@lockedshields.net>
2024-05-16 14:22:08 -0400

    First commit

---
+++ src/p2p_client.rs
@@ -0,0 +1,42 @@
+use std::net::TcpStream;
+use std::io::{Read,Write};
+
+fn write_stream(mut stream: TcpStream){
+    loop {
+        // Declares new string to write to, reads user input, stores it as bytes in array
+        let mut host_input = String::new();
+        print!("Client> ");
+        let _ = std::io::stdout().flush();
+        std::io::stdin().read_line(&mut host_input).expect("Failed to read line.");
+        let message = host_input.as_bytes();
+        //writes message to server
+        stream.write(message).expect("failed to write message.");
+    }
+}
+
+
+fn read_stream(mut stream: TcpStream){
+    loop {
+        let mut _net_buffer = [0u8; 1024];
+        stream.read(&mut _net_buffer).expect("HOLY SHIT WHAT THE FUCK HAPPEND");
+        let incoming_message = String::from_utf8_lossy(&_net_buffer[..]);
+        println!("\n");
+        print!("Server> {}", incoming_message);
+    }
+}
+
+fn outbound_handler(stream: TcpStream){
+    let write_clone: TcpStream = stream.try_clone().expect("Write clone broke");
+    let read_clone: TcpStream = stream.try_clone().expect("Read clone broke");
+    std::thread::spawn(|| read_stream(read_clone));
+    write_stream(write_clone);
+}
+//Entry Point
+fn main(){
+    //connects to local host
+    let connection: TcpStream=TcpStream::connect("127.0.0.1:443").expect("Couldn't connect to server.");
+    let outbound_connection = connection.local_addr().unwrap();
+    println!("Connected to 127.0.0.1:443 from {}", outbound_connection);
+    //Loop for terminal chat window
+    outbound_handler(connection);
+}
